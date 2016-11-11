33550f6af718e7b23b2624ca32d3c5136a1b2784
dmitriy <dmitriy@home>
2016-11-10 20:19:30 -0800

    added tag filtering

--- src/java/rogatkin/test/beejsmultiselect/ux/Books.java
+++ src/java/rogatkin/test/beejsmultiselect/ux/Books.java
@@ -1,6 +1,7 @@
 package rogatkin.test.beejsmultiselect.ux;
 
 import org.aldan3.model.ProcessException;
+import org.aldan3.util.Sql;
 import org.aldan3.data.DODelegator;
 import org.aldan3.model.DataObject;
 import org.aldan3.annot.DataRelation;
@@ -10,8 +11,16 @@ import rogatkin.test.beejsmultiselect.model.BeejsmultiselectModel;
 @DataRelation(query="select ID,NAME,DESCRIPTION,AUTHOR,WRITTEN_ON from book where name like '%:name%' or author like '%:name%'", keys={"name"})
 public class Books extends SqlTabular<DataObject,BeejsmultiselectModel> {
 	
+	@Override
+	protected String getQuery(String query) {
+		String t = getStringParameterValue("tag", "", 0);
+		if (!t .isEmpty())
+			return getTagsSearchQuery(t);
+		return query;
+	}
+	
 	String getTagsSearchQuery(String ...tags) {
-		return "select book.name, book.DESCRIPTION, author, WRITTEN_ON from book join tag_conn on (book.id=book_id) join tag on (tag.id=tag_id) where tag.name in ("
-				+ "'sci-fi','wife')";
+		return "select book.name, book.DESCRIPTION, author, WRITTEN_ON from book join tag_conn on (book.id=book_id) join tag on (tag.id=tag_id) where tag.name in ('"
+				+Sql.escapeQuote(tags[0])+"')";
 	}
 }
